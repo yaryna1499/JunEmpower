36cd7b6ddeea68cf7575c698ceb2d1e9a07e2e74
yaryna1499 <yyyzzz9914@gmail.com>
2023-07-29 16:37:06 +0300

    cors added

--- beck/platform404/JunEmpower/JunEmpower/settings.py
+++ beck/platform404/JunEmpower/JunEmpower/settings.py
@@ -37,8 +37,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     "main",
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+# або
+# CORS_ORIGIN_WHITELIST = [
+#     'http://example.com',
+#     'http://localhost:3000',
+# ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -48,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "JunEmpower.urls"
