3776abfa5ba9b476fd070f803024a86953282ffb
yaryna1499 <yaryna.ostapchuk.email@gmail.com>
2023-08-17 01:47:56 +0300

    api views

--- server/JunEmpower/JunEmpower/urls.py
+++ server/JunEmpower/JunEmpower/urls.py
@@ -1,14 +1,27 @@
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view as drf_yasg_get_schema_view
 
+from drf_yasg import openapi
+
+schema_view_openapi = drf_yasg_get_schema_view(
+    openapi.Info(
+        title="API endpoints for JunEmpower",
+        default_version='v1',
+        description="endpoint to get csrf: GET http://localhost:1443/get_csrf_token/",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("main.urls"), name="main"),
+    path('redoc/', schema_view_openapi.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
--- server/JunEmpower/main/views.py
+++ server/JunEmpower/main/views.py
@@ -1,39 +1,26 @@
-import json
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import generics, viewsets
-from rest_framework.views import APIView
-from .models import CustomUser
-from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
-from .serializer import CustomUserSerializer
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
-from django.http import JsonResponse
 from django.middleware.csrf import get_token
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from django.http import JsonResponse
+from .models import CustomUser
+from .serializer import CustomUserSerializer
 
 
-class CsrfTokenView(View):
-    def get(self, request, *args, **kwargs):
-        csrf_token = get_token(request)
-        return JsonResponse({"csrftoken": csrf_token})
 
+def get_csrf_token(request):
+    csrf_token = get_token(request)
+    return JsonResponse({"csrftoken": csrf_token})
 
 
 class UserApiView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
-
 class UserApiUpdate(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    # permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
-
 
 class UserApiDestroy(generics.RetrieveDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    # permission_classes = (IsAdminOrReadOnly, )
