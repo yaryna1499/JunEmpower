b340471cf44f1d18193d5563f21fdc65a766967c
Yaryna <yaryna.ostapchuk.email@gmail.com>
2023-09-14 14:30:14 +0300

    fix middleware2

--- server-test-version/settings/common.py
+++ server-test-version/settings/common.py
@@ -71,7 +71,7 @@ MIDDLEWARE = [
     # "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    # "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
