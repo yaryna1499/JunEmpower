e062582662c7424b5c1e09229c185bf5db5953a3
Yaryna Ostapchuk <yaryna.ostapchuk.email@gmail.com>
2023-10-19 22:26:12 +0300

    migrations fixed

--- server/docker/python/entrypoint.sh
+++ server/docker/python/entrypoint.sh
@@ -3,13 +3,23 @@
 # exit on error
 set -o errexit
 
+if [ "$DATABASE" = "db_platform404" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
 
 gunicorn --bind 0.0.0.0:8000 JunEmpower.wsgi
-python manage.py collectstatic --no-input
-python manage.py makemigrations
+python manage.py makemigrations main
 python manage.py migrate
+python manage.py collectstatic --no-input
 
-
+exec "$@"
 
 
 
